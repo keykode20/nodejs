adb0df6c0810a2f7b4338b072cdaba705d95cc21
keykode20 <omryemilyohassan@gmail.com>
2018-11-06 21:18:47 +0000

    this is quite powerful, learned how to craete class and a first design pattern, in here you can see even a bit of weak points of node already

--- app5.js
+++ app5.js
@@ -1,11 +1,10 @@
 const EventEmitter = require('events');
-
-const emitter = new EventEmitter();
-
+const Logger = require('./log');
+const logger = new Logger();
 //Register a listerner
 //order is important in this case
-emitter.on('messageLogged',(arg) =>{
+logger.on('messageLogged',(arg) =>{
    console.log('Listener called',arg); 
 });
 
-emitter.emit('messageLogged', {id:1, url:"http://"});
+logger.log('message');
--- log.js
+++ log.js
@@ -1,9 +1,17 @@
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 var testExport = "test";
-function log(message){
+
+class Logger extends EventEmitter{
+   log(message){
     console.log(message);
+       this.emit('messageLogged',{id:1,url:"https://"});
+   } 
+    
 }
 
+
 //adding a method called log in the export object
-module.exports.log = log;
+module.exports = Logger;
 
 module.exports.test = testExport;
