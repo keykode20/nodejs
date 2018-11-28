9244acdb5655feede2b03f25b73bf2cc614d5b9c
keykode20 <omryemilyohassan@gmail.com>
2018-11-28 22:16:16 +0000

    http auth

--- app7.js
+++ app7.js
@@ -6,6 +6,9 @@ var bodyParser = require('body-parser');
 var multer = require('multer'); // v1.0.5
 var upload = multer(); // for parsing multipart/form-data
 const mongo = require('mongodb');
+var basicAuth = require('basic-auth');
+
+
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/mydb";
 MongoClient.connect(url, function(err, db) {
@@ -18,9 +21,22 @@ MongoClient.connect(url, function(err, db) {
     });
 });
 
+var auth = function (req, res, next) {
+  function unauthorized(res) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    return res.send(401);
+  };
 
-
-
+  var user = basicAuth(req);
+  if (!user || !user.name || !user.pass) {
+    return unauthorized(res);
+  };
+  if (user.name === 'admin' && user.pass === 'nimda') {
+    return next();
+  } else {
+    return unauthorized(res);
+  };
+};
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -34,14 +50,11 @@ app.get("/",(req,res)=>{
 });
 
 app.post('/sendMessage', function(req,res){
-  console.log(req.body);
   dbo.collection('contacts').insertOne(req.body);
   res.send('ok');
 });
 
 app.post("/test",(req,res)=>{
-
-  console.log(req.body.message);
   res.sendFile(__dirname+'/static/index.html');
 
   amqp.connect(amqpUrl,function(err,conn){
@@ -56,8 +69,8 @@ app.post("/test",(req,res)=>{
     });
   });
 });
-
-app.get('/backoffice',(req,res)=>{
+//https://davidbeath.com/posts/expressjs-40-basicauth.html
+app.get('/backoffice', auth,(req,res)=>{
   res.sendFile(__dirname+'/backoffice/index.html');
 });
 
@@ -65,7 +78,6 @@ app.post('/loadPage',(req,res)=>{
   var messages = new Array();
   dbo.collection("contacts").find({}).toArray(function(err,result){
         result.forEach(function(element){
-          console.log(element);
           messages.push(element);
           //  messages.push(response);
     });
